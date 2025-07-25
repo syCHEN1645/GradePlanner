a0b48c0c296e2e3bf5c422c52a153a8029875970
syCHEN1645 <e1121133@u.nus.edu>
2025-07-25 20:11:11 +0800

    Add Method to Calculate Total Credit in List

--- src/CourseList.java
+++ src/CourseList.java
@@ -42,6 +42,14 @@ public abstract class CourseList {
         }
         return totalCredit == 0 ? 0 : totalGrade / totalCredit;
     }
+    
+    public int calculateSemCredit() {
+        int totalCredit = 0;
+        for (Course c : courses) {
+            totalCredit += c.getCredit();
+        }
+        return totalCredit;
+    }
 
     public void editCourse(int index, int credit, float grade) {
         if (index < courses.size()) {
