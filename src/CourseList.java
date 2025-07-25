32e506879d0f96b6e9d40c0eb715a5b5ea4c1093
syCHEN1645 <e1121133@u.nus.edu>
2025-07-25 19:42:56 +0800

    Remove Past & Future CourseList and Merge into CourseList

--- src/CourseList.java
+++ src/CourseList.java
@@ -2,6 +2,15 @@ import java.util.List;
 
 public abstract class CourseList {
     protected List<Course> courses;
+    boolean fixed;
+
+    public boolean isFixed() {
+        return fixed;
+    }
+
+    public void setFixed(boolean fixed) {
+        this.fixed = fixed;
+    }
 
     public void addCourse(Course course) {
         courses.add(course);
@@ -13,5 +22,24 @@ public abstract class CourseList {
         }
     }
 
-    public abstract void listCourses();
+    public void listCourses() {
+        for (int i = 0; i < courses.size(); i++) {
+            System.out.println(
+                    "<" + (i + 1) + "> " +
+                            courses.get(i).getCredit() + " MCs" + "\t" +
+                            courses.get(i).getGrade() + " GPA" + "\t" +
+                            courses.get(i).getInfo()
+            );
+        }
+    }
+
+    public float calculateSemGrade() {
+        float totalGrade = 0;
+        float totalCredit = 0;
+        for (Course c : courses) {
+            totalGrade += c.getGrade() * c.getCredit();
+            totalCredit += c.getCredit();
+        }
+        return totalCredit == 0 ? 0 : totalGrade / totalCredit;
+    }
 }
