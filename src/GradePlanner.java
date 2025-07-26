e68a0d1972935c856741e7951e43b156c36798d1
syCHEN1645 <e1121133@u.nus.edu>
2025-07-26 12:47:34 +0800

    Set Basic Params

--- src/GradePlanner.java
+++ src/GradePlanner.java
@@ -1,3 +1,20 @@
 public class GradePlanner {
     protected static CoursePlan plan = new CoursePlan();
+    protected static float target;
+
+    public static float getTarget() {
+        return target;
+    }
+
+    public static void setTarget(float target) {
+        GradePlanner.target = target;
+    }
+
+    public static void minimumPlan() {
+        //
+    }
+
+    public static void minimumAverageGrade() {
+        //
+    }
 }
