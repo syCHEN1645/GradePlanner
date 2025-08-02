7801d77a0e1d54a819c3161f1718cd8c93600013
syCHEN1645 <e1121133@u.nus.edu>
2025-08-02 13:35:37 +0800

    Add Command Execution Flow

--- src/GradePlanner.java
+++ src/GradePlanner.java
@@ -5,23 +5,48 @@ public class GradePlanner {
     protected static ArrayList<CoursePlan> plans = new ArrayList<>();
     protected static CoursePlan currentPlan;
     protected static Scanner scanner = new Scanner(System.in);
+    protected static boolean active;
 
-//    public GradePlanner() {
-//        if (plans.isEmpty()) {
-//            addCoursePlan();
-//        }
-//        currentPlan = plans.get(0);
-//    }
+    public GradePlanner() {
+        if (plans.isEmpty()) {
+            addCoursePlan();
+        }
+        currentPlan = plans.get(0);
+        active = true;
+    }
 
     public static void startProgram() {
-        while (true) {
+        while (active) {
             String input = scanner.nextLine();
             Command command = CommandParser.parse(input);
-            CommandParser.execute(command);
+            execute(command);
         }
     }
 
-    public static void addCoursePlan() {
+    protected static void execute(Command command) {
+        if (command.getName().equals(Command.LIST)) {
+            list((CommandList) command);
+        } else if (command.getName().equals(Command.EXIT) || command.getName().equals(Command.QUIT)) {
+            quit((CommandQuit) command);
+        } else {
+            // invalid command
+            invalidate();
+        }
+    }
+
+    protected static void list(CommandList command) {
+        currentPlan.listPlan();
+    }
+
+    protected static void quit(CommandQuit command) {
+        active = false;
+    }
+
+    protected static void invalidate() {
+        System.out.println("Invalid command");
+    }
+
+    protected void addCoursePlan() {
         plans.add(new CoursePlan());
     }
 
